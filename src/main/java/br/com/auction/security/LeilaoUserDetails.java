a4f07edd75ec45ed0852f684c27070bf4802871e
wagner maidson <wagner.rmaidson@gmail.com>
2021-10-22 21:04:49 -0300

    security

---
+++ src/main/java/br/com/auction/security/LeilaoUserDetails.java
@@ -0,0 +1,60 @@
+package br.com.auction.security;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import br.com.auction.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class LeilaoUserDetails implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	private Usuario user;
+
+	public LeilaoUserDetails(Usuario user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+		return Arrays.asList(authority);
+	}
+
+	public Usuario getUsuario() {
+		return this.user;
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getSenha();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getNome();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
---
+++ src/main/java/br/com/auction/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package br.com.auction.security;
+
+import br.com.auction.dao.UsuarioDao;
+import br.com.auction.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UsuarioDao usuarioDao;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario user = usuarioDao.buscarPorUsername(username);
+
+		if (user == null) {
+			throw new UsernameNotFoundException("Usuario nao encontrado");
+		}
+
+		return new LeilaoUserDetails(user);
+	}
+
+}
